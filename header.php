1cd5d677556a84aa039ddc4221eefc84e1d09caa
salik iqbal <sisalik84@gmail.com>
2024-11-24 22:55:46 +0530

    First Commit

---
+++ header.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from uithemez.com/i/restfolio/home1_restaurant_original/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 14 Oct 2024 10:13:52 GMT -->
+<head>
+    <!-- Metas -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="keywords" content="HTML5 Template Artech Multi-Purpose themeforest">
+    <meta name="description" content="Artech - Multi-Purpose HTML5 Template">
+    <meta name="author" content="">
+
+    <!-- Title  -->
+    <title>Restfolio | Restaurant</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/img/fav.png" title="Favicon" sizes="16x16">
+
+    <!-- bootstrap 5 -->
+    <link rel="stylesheet" href="../common/assets/css/lib/bootstrap.min.css">
+
+    <!-- bootstrap 5 rtl -->
+    <!-- <link rel="stylesheet" href="../common/assets/css/lib/bootstrap.rtl.min.css"> -->
+
+    <!-- font family -->
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&amp;display=swap" rel="stylesheet">
+    
+    <!-- fontawesome icons  -->
+    <link rel="stylesheet" href="../common/assets/css/lib/all.css">
+    <!-- animate css  -->
+    <link rel="stylesheet" href="../common/assets/css/lib/animate.css">
+    <!-- fancybox popup  -->
+    <link rel="stylesheet" href="../common/assets/css/lib/jquery.fancybox.css">
+    <!-- lity popup  -->
+    <link rel="stylesheet" href="../common/assets/css/lib/lity.css">
+    <!-- swiper slider  -->
+    <link rel="stylesheet" href="../common/assets/css/lib/swiper8.min.css">
+
+    <!-- common style -->
+    <link rel="stylesheet" href="../common/assets/css/common_style.css">
+    
+
+    <!-- home style -->
+    <link rel="stylesheet" href="assets/css/home_1_style.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script>
+        window.ga = window.ga || function() {
+          (ga.q = ga.q || []).push(arguments)
+        };
+        ga.l = +new Date;
+        ga('create', 'UA-105392568-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+
+</head>
+
+<body class="home-st1">
+
+    <canvas class="banner_canvas" id="canvas_banner"></canvas>
+
+    <!-- ====== Start Loading ====== -->
+
+    <div class="loader-wrap">
+        <svg viewBox="0 0 1000 1000" preserveAspectRatio="none" width="100%" height="100%">
+            <defs>
+              <linearGradient id="path-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" style="stop-color: #000" />
+                <stop offset="100%" style="stop-color: #555" />
+              </linearGradient>
+            </defs>
+            <path id="svg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z" fill="url(#path-gradient)"></path>
+        </svg>
+
+        <div class="loader-wrap-heading">
+            <div class="load-logo cont mb-50">
+                <img src="assets/img/logo.svg" alt="" class="">
+            </div>
+            <div class="load-text">
+                <span>R</span>
+                <span>E</span>
+                <span>S</span>
+                <span>T</span>
+                <span>F</span>
+                <span>o</span>
+                <span>L</span>
+                <span>I</span>
+                <span>O</span>
+            </div>
+        </div>
+    </div>
+
+
+    <!--  Start fixed menu  -->
+    <div class="fixed-menu">
+        <div class="food-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <a href="https://uithemez.com/i/restfolio/inner-pages/portfolio_single.html" class="dish-card">
+                        <div class="img">
+                            <img src="assets/img/menu_dishes/chicken.png" alt="">
+                        </div>
+                    </a>
+                </div>
+                <div class="swiper-slide">
+                    <a href="https://uithemez.com/i/restfolio/inner-pages/portfolio_single.html" class="dish-card">
+                        <div class="img">
+                            <img src="assets/img/menu_dishes/chicken_peppers.png" alt="">
+                        </div>
+                    </a>
+                </div>
+                <div class="swiper-slide">
+                    <a href="https://uithemez.com/i/restfolio/inner-pages/portfolio_single.html" class="dish-card">
+                        <div class="img">
+                            <img src="assets/img/menu_dishes/meat.png" alt="">
+                        </div>
+                    </a>
+                </div>
+                <div class="swiper-slide">
+                    <a href="https://uithemez.com/i/restfolio/inner-pages/portfolio_single.html" class="dish-card">
+                        <div class="img">
+                            <img src="assets/img/menu_dishes/nachos_guacamole.png" alt="">
+                        </div>
+                    </a>
+                </div>
+                <div class="swiper-slide">
+                    <a href="https://uithemez.com/i/restfolio/inner-pages/portfolio_single.html" class="dish-card">
+                        <div class="img">
+                            <img src="assets/img/menu_dishes/pasta.png" alt="">
+                        </div>
+                    </a>
+                </div>
+                <div class="swiper-slide">
+                    <a href="https://uithemez.com/i/restfolio/inner-pages/portfolio_single.html" class="dish-card">
+                        <div class="img">
+                            <img src="assets/img/menu_dishes/salad.png" alt="">
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="food-text-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="text-card">
+                        <h2> chicken </h2>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="text-card">
+                        <h2> chicken peppers </h2>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="text-card">
+                        <h2> meat </h2>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="text-card">
+                        <h2> nachos guacamole </h2>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="text-card">
+                        <h2> pasta </h2>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="text-card">
+                        <h2> salads </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+        <div class="circle"></div>
+        <span class="clss"><i class="fas fa-times-circle"></i></span>
+    </div>
+    <!--  End fixed menu  -->
+
+
+    <div id="scrollsmoother-container">
+
+
+        <!--  Start navbar  -->
+        <div class="tc-topNav-st1">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-lg-4">
+                        <p class="welcome"> Welcome to our Pinkhashtag cafe </p>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="logo th-50 my-4 my-lg-0">
+                            <img src="assets/img/cafelogo.png" alt="" class="img-contain">
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="social-links text-center text-lg-end">
+                            <a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i>
+                            </a>
+                            <a href="#"> <i class="fa fa-linkedin" aria-hidden="true"></i>
+                            </a>
+                            <a href="#"> <i class="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                            <a href="#"> <i class="fa fa-twitter" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ------------- -->
+        <nav class="navbar navbar-expand-lg navbar-dark tc-navbar-st1">
+            <div class="container-fluid">
+                <div class="nav-icons">
+                    <div class="menu-item">
+                        <a class="nav-link side_menu_btn" href="#"> Menu </a>
+                    </div>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse order-last order-lg-0" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto sub-font">
+                       
+                    <li >
+                            <a class="nav-link dropdown-toggle" href="index.php" role="button"  aria-expanded="false">
+                                Homes
+                            </a>
+                        </li >
+                            
+                       
+                        <li class="nav-item">
+                            <a class="nav-link" href="rangtalesart.php">Rangtales Art</a>
+                        </li>
+                    </ul>
+                </div>
+                <a href="#" class="butn bg-red1 text-white rounded-pill size-1 d-none d-lg-inline-block"> <span> Book a Table + </span> </a>
+            </div>
+        </nav>
+        <!--  End navbar  -->
